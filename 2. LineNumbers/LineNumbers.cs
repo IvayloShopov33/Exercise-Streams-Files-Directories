fed5cfe11b2ba314a22d8811cd92916ba42e9d98
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-25 01:40:21 +0300

    Add files via upload

---
+++ 2. LineNumbers/LineNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            string output = string.Empty;
+            int linesCounter = 1;
+            foreach (string line in lines)
+            {
+                int lettersCount = 0;
+                int punctuationMarksCount = 0;
+                foreach (char @char in line)
+                {
+                    if (char.IsLetter(@char))
+                    {
+                        lettersCount++;
+                    }
+                    else if (char.IsPunctuation(@char))
+                    {
+                        punctuationMarksCount++;
+                    }
+                }
+
+                output += $"Line {linesCounter}: {line} ({lettersCount})({punctuationMarksCount})";
+                output += "\n";
+                linesCounter++;
+            }
+
+            File.WriteAllText(outputFilePath, output);
+        }
+    }
+}
