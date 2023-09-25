8d9687bb992bc6f88cc8f34971153a33e09001f8
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-26 01:24:23 +0300

    Add files via upload

---
+++ 3. CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using FileStream fileStreamReader = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read, FileShare.None);
+            using FileStream fileStreamWriter = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write, FileShare.None);
+
+            byte[] bufferData = new byte[fileStreamReader.Length];
+            fileStreamReader.Read(bufferData);
+            fileStreamWriter.Write(bufferData);
+
+        }
+    }
+}
