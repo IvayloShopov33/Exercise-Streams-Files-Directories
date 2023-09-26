c5b9cd7d755e4819dcee04e0dc81bcc148f580a4
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-27 01:19:09 +0300

    Add files via upload

---
+++ 5. CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace CopyDirectory
+{
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            string[] filesToMove = Directory.GetFiles(inputPath);
+            if (File.Exists(outputPath))
+            {
+                File.Delete(outputPath);
+            }
+
+            File.Create(outputPath);
+            foreach (string file in filesToMove)
+            {
+                File.Move(file, outputPath);
+            }
+        }
+    }
+}
