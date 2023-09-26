bb9643023c4895cd7ffaa9148f4645446adc151d
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-27 01:21:29 +0300

    Add files via upload

---
+++ 6. ZipAndExtract/ZipAndExtract .cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace ZipAndExtract
+{
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFile = @"C:\Users\plame\Desktop\My folder";
+            string zipArchiveFile = @"..\..\..\archive.zip";
+            string extractedFile = @"..\..\..\result";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            ExtractFileFromArchive(zipArchiveFile, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            ZipFile.CreateFromDirectory(inputFilePath, zipArchiveFilePath);
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string outputFilePath)
+        {
+            ZipFile.ExtractToDirectory(zipArchiveFilePath, outputFilePath);
+        }
+    }
+}
